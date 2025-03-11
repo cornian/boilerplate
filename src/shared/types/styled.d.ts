3c54749e1f186963b53d8f6fc06fa9c54baa4d30
jinhyunwoo-FE <jkr2505@naver.com>
2025-03-11 13:09:01 +0900

    chore: install styled-components

---
+++ src/shared/types/styled.d.ts
@@ -0,0 +1,17 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      primary: string;
+      secondary: string;
+      background: string;
+      text: string;
+    };
+    spacing: {
+      small: string;
+      medium: string;
+      large: string;
+    };
+  }
+}
